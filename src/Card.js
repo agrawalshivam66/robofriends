b00f7e906eefb31c92916864d9029be86751b30a
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-01-27 00:24:52 +0530

    learning react

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () =>{
+	return(
+		<div>
+		<img src=''/>
+		<div>
+		<h2>Shivam Agrawal</h2>
+		<p>agrawalshivam66@gmail.com</p>
+		</div>
+		</div>
+	);
+}
+
+export default Card;
