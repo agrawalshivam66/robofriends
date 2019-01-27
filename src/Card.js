4d6efa97312586612b99615e53df46684d61a20a
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-01-27 15:59:22 +0530

    update

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Card = () =>{
-	return(
-		<div>
-		<img src=''/>
-		<div>
-		<h2>Shivam Agrawal</h2>
-		<p>agrawalshivam66@gmail.com</p>
-		</div>
-		</div>
-	);
+const Card = ({ name, email, id }) => {
+  return (
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
